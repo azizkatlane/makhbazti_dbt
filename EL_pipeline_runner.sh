0ffef0865e967e0080c49f7ada0e3cbb857f2d26
azizkatlane <azizkatlane@gmail.com>
2024-11-13 15:00:43 +0100

    env variables

--- EL_pipeline_runner.sh
+++ EL_pipeline_runner.sh
@@ -6,7 +6,7 @@ cd ~/makhbazti_dbt
 mkdir -p logs 
 
 
-echo "ETL pipeline started at: $(date +"%d-%m-%y %T")"
+echo "EXTRACT LOAD  EL pipeline started at: $(date +"%d-%m-%y %T")"
 
 python mongodb_pipeline.py 2>&1 | tee logs/mongodb_pipeline.log
 echo "EXTRACT LOAD  completed at: $(date +"%d-%m-%y %T")"
