f19f072a8e3c5f2352d99243e99aae59658a6ad4
azizkatlane <azizkatlane@gmail.com>
2024-11-13 15:46:50 +0100

    corrected profiles path in shell script

--- dbt_modeling.sh
+++ dbt_modeling.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
+# Navigate to the project directory
 cd ~/makhbazti_dbt/MAKHBAZTI_DBT
 
+# Run dbt commands with the updated profiles directory path
+dbt seed --select "Tunisia_Holidays" --profiles-dir ~/makhbazti_dbt/MAKHBAZTI_DBT
+dbt snapshot --profiles-dir ~/makhbazti_dbt/MAKHBAZTI_DBT
 
-dbt seed --select "Tunisia_Holidays" --profiles-dir ~/.dbt/
-
-
-dbt snapshot --profiles-dir ~/.dbt/
-
-# Run the dbt transformation
-dbt run --profiles-dir ~/.dbt/ 2>&1 | tee logs/dbt_transform.log
+# Run the dbt transformation and log output
+dbt run --profiles-dir ~/makhbazti_dbt/MAKHBAZTI_DBT 2>&1 | tee logs/dbt_transform.log
